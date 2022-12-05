afa1b6eca3fa564225dc12e15213f6cd548862fa
Louis WATRIN <louis.watrin@redfroggy.fr>
2022-12-05 10:59:37 +0100

    feat: add count in localstorage and sort

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,19 @@
-import {Injector, NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {createCustomElement} from '@angular/elements';
-import {StellianceConnectWidgetComponent} from './stelliance-connect-widget/stelliance-connect-widget.component';
+import { Injector, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { createCustomElement } from '@angular/elements';
+import { StellianceConnectWidgetComponent } from './stelliance-connect-widget/stelliance-connect-widget.component';
+import { OrderByPipe } from './utils/order-by-pipe';
 
 @NgModule({
-  declarations: [
-    StellianceConnectWidgetComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [StellianceConnectWidgetComponent, OrderByPipe],
+  imports: [BrowserModule],
   providers: [],
-  entryComponents: [StellianceConnectWidgetComponent]
+  entryComponents: [StellianceConnectWidgetComponent],
 })
 export class AppModule {
   constructor(injector: Injector) {
-    const widgetCustomElement = createCustomElement(StellianceConnectWidgetComponent, {injector});
-    customElements.define('stc-app-widget', widgetCustomElement)
+    const widgetCustomElement = createCustomElement(StellianceConnectWidgetComponent, { injector });
+    customElements.define('stc-app-widget', widgetCustomElement);
   }
   ngDoBootstrap() {}
-
 }
