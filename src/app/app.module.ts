380b929f15f832228eb4a55584a73e25d223a516
Louis WATRIN <louis.watrin@redfroggy.fr>
2022-12-05 11:04:09 +0100

    Revert "feat: add count in localstorage and sort v2"
    This reverts commit ecd2a1ebbb2a6265556b07f1ade999345a6394c6.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { createCustomElement } from '@angular/elements';
 import { StellianceConnectWidgetComponent } from './stelliance-connect-widget/stelliance-connect-widget.component';
+import { OrderByPipe } from './utils/order-by-pipe';
 
 @NgModule({
-  declarations: [StellianceConnectWidgetComponent],
+  declarations: [StellianceConnectWidgetComponent, OrderByPipe],
   imports: [BrowserModule],
   providers: [],
   entryComponents: [StellianceConnectWidgetComponent],
