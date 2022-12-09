d2f0c69aeb16cccba57c683bc0146366d1b37647
Michael Desigaud <michael.desigaud@redfroggy.fr>
2022-12-09 09:33:40 +0100

    ci: copy assets in githubpages

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-npm run build && cp src/styles.css preview/ && cp dist/angular-element-app/output.js preview/angularapp.js
+npm run build
+&& cp src/assets/ preview/
+&& cp src/styles.css preview/
+&& cp dist/angular-element-app/output.js preview/angularapp.js
