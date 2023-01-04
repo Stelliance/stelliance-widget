b2443f4bbe196c6d60ce387c6ec37c30bedc9d2e
Michael Desigaud <michael.desigaud@redfroggy.fr>
2023-01-04 14:04:17 +0100

    feat: generate redirect url from conf + generated code challenge

--- src/shared/util/code-challenge.util.ts
+++ src/shared/util/code-challenge.util.ts
@@ -6,7 +6,6 @@ export class CodeChallengeUtil {
     const data = encoder.encode((Math.random() + 1).toString(36).substring(7));
     return window.crypto.subtle.digest('SHA-256', data).then((digest) => {
       const base64Digest = base64encode(digest);
-      // you can extract this replacing code to a function
       return base64Digest.replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
     });
   }
