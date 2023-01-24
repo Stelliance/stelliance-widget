4e6e59e2fc409cc53774a50194c7c44bb6af162b
Michael Desigaud <michael.desigaud@redfroggy.fr>
2023-01-24 11:04:08 +0100

    chore: fix conf

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-npm run build && cp src/styles.css preview/ && cp dist/stc-widget/output.js preview/stc-widget.js
+npm run build && cp -r src/assets/ preview/ && cp src/styles.css preview/ && cp dist/stc-widget/output.js preview/stc-widget.js
